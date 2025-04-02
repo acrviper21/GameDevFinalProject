805dd341a16b6f4c2cff6a7d79e95e9e6b27d1ac
acrviper21 <ecrash789@gmail.com>
2025-04-01 20:40:23 -0500

    Updated TutorialDialogue.cs
    Fixed method for detecting type of controller using

--- FinalProject/Assets/Scripts/DialogueScripts/TutorialDialogue.cs
+++ FinalProject/Assets/Scripts/DialogueScripts/TutorialDialogue.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TutorialDialogue : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class TutorialDialogue : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if(Input.GetJoystickNames().Length > 0)
+        if(Gamepad.all.Count > 0)
         {
             //usingController = true;
             SetDialogueForController();
