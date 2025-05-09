2949ea4b817a2a15cca090a08db83cd7f17fd0da
acrviper21 <ecrash789@gmail.com>
2025-05-09 15:21:06 -0500

    Added resolutions to choose from

--- FinalProject/Assets/Scripts/MainMenuScripts/ScreenSettings.cs
+++ FinalProject/Assets/Scripts/MainMenuScripts/ScreenSettings.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using System;
+using TMPro;
 
 public class ScreenSettings : MonoBehaviour
 {
+    [SerializeField] TMP_Dropdown resolutionDropDown;
+    Resolution[] resolutions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        resolutions = Screen.resolutions;
+        Resolution currentResolution = Screen.currentResolution;
+
+        foreach(Resolution resolution in resolutions)
+        {
+            resolutionDropDown.options.Add(new TMP_Dropdown.OptionData(resolution.ToString()));
+        }
+
+        resolutionDropDown.value = Array.IndexOf(resolutions, currentResolution);
+    }
+
+    public void SetResolution()
+    {
+        int currentIndex = resolutionDropDown.value;
+        Resolution rez = resolutions[currentIndex];
+        Screen.SetResolution(rez.width, rez.height, Screen.fullScreen);
     }
 
     // Update is called once per frame
